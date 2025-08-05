98b4d6b3be74f6f6f3867173d343371bcb34bf4c
Bradley Losavio <blosavio@sagevisuals.com>
2025-08-05 20:29:17 +0800

    Updates to incorporate re-naming from `measure` and `display` namespaces.

--- src/fastester/core.clj
+++ src/fastester/core.clj
@@ -4,9 +4,9 @@
   High-level workflow:
 
   1. Write benchmarks in a dedicated namespace.
-  2. Run performance benchmarks with, e.g., [[do-all-performance-tests]].
+  2. Run performance benchmarks with, e.g., [[run-all-benchmarks]].
   3. Create an html document with charts and tables with, e.g.,
-  [[generate-all-displays]]."
+  [[generate-documents]]."
   (:require
    [fastester.measure :refer [run-all-benchmarks]]
    [fastester.display :refer [generate-documents]]))
@@ -16,7 +16,7 @@
   "Run all benchmarks and generate an html document that displays comparisons to
   previous versions.
 
-  See [[do-all-performance-tests]] and [[generate-all-displays]]"
+  See [[run-all-benchmarks]] and [[generate-documents]]"
   {:UUIDv4 #uuid "e26d0308-229f-4f2d-b24f-60ff9e59d109"}
   []
   (do
